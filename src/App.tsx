ffb713c48ff8508d0140e1d874bc9abfcb8730ba
adunham95 <adunham95@gmail.com>
2021-09-17 08:58:46 -0400

    Added dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,15 @@ import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Header from './components/Header'
-import {ModalProvider} from "./components/ModalContext.jsx"
+import {ModalProvider} from "./components/ModalContext.jsx";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Dashboard from "./views/Dashboard.jsx"
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,8 +18,19 @@ function App() {
   return (
     <div className="App">
       <ModalProvider>
+        <Router>
       <div className="flex flex-col"></div>
       <Header/>
+      <Switch>
+          <Route exact path="/">
+            <Dashboard/>
+          </Route>
+          <Route exact path="*">
+            <h1>404 Page</h1>
+            {/* <Home /> */}
+          </Route>
+        </Switch>
+      </Router>
       </ModalProvider>
     </div>
   )
