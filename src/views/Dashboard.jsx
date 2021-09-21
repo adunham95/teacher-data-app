620c7c28139b3e1fca97deef9e6d0b8567b2e883
adunham95 <adunham95@gmail.com>
2021-09-21 11:43:05 -0400

    Added some styling

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -27,22 +27,31 @@ const Dashboard = () => {
 
   return (
     <div>
-      <h1>Home</h1>
-      <div>
-        <div>
-          {groupData.length}
+      <header
+        className="bg-white border-b-2"
+      >
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
         </div>
-        <div>
-          {studentData.length}
+      </header>
+      <main className="container p-1">
+        <div className="flex">
+          <div className="border-2 border-blue-500 rounded p-4 flex flex-col justify-center content-center w-1/5 mr-1 mb-1">
+            <span className="text-center">Groups</span>
+            <span className="text-center font-bold text-3xl">{groupData.length}</span>
+          </div>
+          <div className="border-2 border-blue-500 rounded p-4 flex flex-col justify-center content-center w-1/5 mr-1 mb-1">
+            <span className="text-center">Students</span>
+            <span className="text-center font-bold text-3xl">{studentData.length}</span>
+          </div>
         </div>
-      </div>
-      <div>
-        <GroupCard
-          color="bg-green-500"
-          groupName="New Group"
-          onClick={() => setModalID('newBlock')}
-        />
-        {
+        <div className="flex flex-wrap">
+          <GroupCard
+            color="border-green-500"
+            groupName="New Group"
+            onClick={() => setModalID('newBlock')}
+          />
+          {
           groupData.map((g) => (
             <GroupCard
               key={g.id}
@@ -50,7 +59,8 @@ const Dashboard = () => {
             />
           ))
         }
-      </div>
+        </div>
+      </main>
       <Modal id="newBlock">
         <div className="bg-white">
           <h1 className="pt-4 px-4">New Group</h1>
@@ -65,23 +75,30 @@ const Dashboard = () => {
 function GroupCard({
   id = '', color, groupName, onClick,
 }) {
-  console.log(onClick);
+  const classes = {
+    inner: `${getColorClassName(color, 'border')} p-5 text-center border-2 transition duration-150  border-opacity-75 hover:border-opacity-100 rounded `,
+    outter: 'p-1 w-1/5',
+  };
   if (onClick === undefined) {
     return (
       <Link
         to={`/group/${id}`}
-        className={getColorClassName(color, 'bg')}
+        className={classes.outter}
       >
-        {groupName}
+        <div className={classes.inner}>
+          {groupName}
+        </div>
       </Link>
     );
   }
   return (
     <button
       onClick={onClick}
-      className={getColorClassName(color, 'bg')}
+      className={classes.outter}
     >
-      {groupName}
+      <div className={classes.inner}>
+        {groupName}
+      </div>
     </button>
   );
 }
