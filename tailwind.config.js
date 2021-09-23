f8aa71605a7a1147ea1ffa85ffe4a6466fd78779
adunham95 <adunham95@gmail.com>
2021-09-23 11:43:52 -0400

    Added color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,26 @@
 const colors = require('./data/colorData.json');
 
 const colorObject = {};
+const safeColorList = [];
 
 colors.forEach((c) => {
   colorObject[c.className] = c.color;
+  safeColorList.push(`bg-${c.className}`);
+  safeColorList.push(`ring-${c.className}`);
+  safeColorList.push(`border-${c.className}`);
+  safeColorList.push(`text-${c.className}`);
 });
+
+console.log(safeColorList);
 
 module.exports = {
   purge: [
     './src/**/*.html',
     './src/**/*.tsx',
     './src/**/*.jsx',
+  ],
+  safelist: [
+    ...safeColorList,
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
