0541f8c865f93bf4dcf56fc7a4f39dbe237aedd0
adunham95 <adunham95@gmail.com>
2021-09-23 06:56:07 -0400

    Added student list component

--- src/views/Students.tsx
+++ src/views/Students.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { getAllStudents } from '../functions/students';
 import PageHeader from '../components/PageHeader';
-import GroupChip from '../components/GroupChip';
 import { useModal } from '../components/ModalContext';
 import Modal from '../components/Modal';
 import StudentForm from '../components/StudentForm';
+import StudentList from '../components/StudentList';
+import { StudentInterface } from '../interface';
 
 const Students = () => {
-  const [allStudents, setAllStudent] = useState<string[]>([]);
+  const [allStudents, setAllStudent] = useState<StudentInterface[]>([]);
   const [searchTerm, setSearchTerm] = useState('');
   const { setModalID }: any = useModal();
 
@@ -45,59 +45,7 @@ const Students = () => {
             </button>
           </div>
         </div>
-        <div className="flex flex-col">
-          <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-              <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                <table className="min-w-full divide-y divide-gray-200">
-                  <thead className="bg-gray-50">
-                    <tr>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Name
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Groups
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="bg-white divide-y divide-gray-200">
-                    {allStudents.filter((p:any) => `${p.firstName} ${p.lastName}`.includes(searchTerm)).map((person:any) => (
-                      <tr key={person.id}>
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          <div className="flex items-center">
-                            <div className="ml-4">
-                              <Link
-                                className='className="text-sm font-medium text-gray-900'
-                                to={`/students/${person.id}`}
-                              >
-                                {person.firstName}
-                                {' '}
-                                {person.lastName}
-                              </Link>
-                            </div>
-                          </div>
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          {
-                                person.groups.map((g) => (
-                                  <GroupChip {...g} />
-                                ))
-                            }
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-        </div>
+        <StudentList students={allStudents.filter((p:any) => `${p.firstName} ${p.lastName}`.toLowerCase().includes(searchTerm.toLowerCase()))} />
       </main>
       <Modal id="newStudent">
         <StudentForm onSave={(stu: any) => savedStudent(stu)} />
